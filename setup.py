951616c9789689d572d52ee2764b0a3ca2bd0bb3
Ximin Luo <infinity0@gmx.com>
2013-11-01 18:04:25 +0000

    add txsocksx to direct deps, and zope.interface to indirect deps, in py2exe setup script - works around py2exe on wine silently not finding them

--- setup.py
+++ setup.py
@@ -1,13 +1,23 @@
 from distutils.core import setup
 import py2exe
 
+# if py2exe complains "can't find P", try one of the following workarounds:
+#
+# a. py2exe doesn't support zipped eggs - http://www.py2exe.org/index.cgi/ExeWithEggs
+#  You should give the --always-unzip option to easy_install, or you can use setup.py directly
+#  $ python setup.py install --record install.log --single-version-externally-managed
+#  Don't forget to remove the previous zipped egg.
+#
+# b. Add an empty __init__.py to the P/ top-level directory, if it's missing
+#  - this is due to a bug (or misleading documentation) in python's imp.find_module()
+
 setup(
     console=["obfs-flash-client"],
     zipfile="py2exe-obfs-flash-client.zip",
     options={
         "py2exe": {
-            "includes": ["pyptlib", "twisted"],
-            "packages": ["ometa", "terml"],
+            "includes": ["pyptlib", "twisted", "txsocksx"],
+            "packages": ["ometa", "terml", "zope.interface"],
         },
     },
 )
